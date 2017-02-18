0ae121dd38fa6249706b4ecc4c3fd4092bb81876
Canis (Joshua Jacobson) <linktheherooftime2@gmail.com>
2017-02-17 22:16:43 -0700

    Correct terminology
    stick -> axis

--- src/org/usfirst/frc/team2647/robot/Input.java
+++ src/org/usfirst/frc/team2647/robot/Input.java
@@ -15,7 +15,7 @@ public final class Input {
 	private static class Joy {
 		private Joystick joystick_;
 		private java.util.HashMap<Integer, Boolean> buttons_;
-		private java.util.HashMap<Integer, Double> sticks_;
+		private java.util.HashMap<Integer, Double> axes_;
 		
 		public Joy(int port) {
 			joystick_ = new Joystick(port);
@@ -25,17 +25,17 @@ public final class Input {
 			if (!(buttons_.containsKey(key))) buttons_.put(key, joystick_.getRawButton(key));
 			return buttons_.get(key);
 		}
-		public double getStick(int key) {
-			if (!(sticks_.containsKey(key))) sticks_.put(key, joystick_.getRawAxis(key));
-			return sticks_.get(key);
+		public double getAxis(int key) {
+			if (!(axes_.containsKey(key))) axes_.put(key, joystick_.getRawAxis(key));
+			return axes_.get(key);
 		}
 		
 		public void update() {
 			for (int key : buttons_.keySet()) {
 				buttons_.put(key, joystick_.getRawButton(key));
 			}
-			for (int key : sticks_.keySet()) {
-				sticks_.put(key, joystick_.getRawAxis(key));
+			for (int key : axes_.keySet()) {
+				axes_.put(key, joystick_.getRawAxis(key));
 			}
 		}
 	}
@@ -52,9 +52,9 @@ public final class Input {
 		checkJoystick(joyPort);
 		return joysticks_.get(joyPort).getButton(key);
 	}
-	public static double getStick(int joyPort, int key) {
+	public static double getAxis(int joyPort, int key) {
 		checkJoystick(joyPort);
-		return joysticks_.get(joyPort).getStick(key);
+		return joysticks_.get(joyPort).getAxis(key);
 	}
 	public static void update() {
 		for (Joy joystick : joysticks_.values()) {
