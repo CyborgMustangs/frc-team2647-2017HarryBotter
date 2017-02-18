867b1ba6ae5e963f0c8bc840de0b24a9d39f4aa0
Canis (Joshua Jacobson) <linktheherooftime2@gmail.com>
2017-02-18 01:10:36 -0700

    Give Joy its own class.

---
+++ src/org/usfirst/frc/team2647/robot/Joy.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team2647.robot;
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+private static class Button {
+		public Button(int buttonNum, boolean buttonState) {
+			num = buttonNum;
+			state = buttonState;
+		}
+		public int num;
+		public boolean state;
+	}
+	private static class Axis {
+		public Axis(int axisNum, double axisState) {
+			num = axisNum;
+			state = axisState;
+		}
+		public int num;
+		public double state;
+	}
+	
+	private static class Joy {
+		private Joystick joystick_;
+		private java.util.HashMap<String, Button> buttons_;
+		private java.util.HashMap<String, Axis> axes_;
+		
+		public Joy(int port) {
+			joystick_ = new Joystick(port);
+		}
+		public void setButton(String name, int buttonNum) {
+			buttons_.put(name, new Button(buttonNum, joystick_.getRawButton(buttonNum)));
+		}
+		public void setAxis(String name, int axisNum) {
+			axes_.put(name, new Axis(axisNum, joystick_.getRawAxis(axisNum)));
+		}
+		public boolean getButton(String name) {
+			if (buttons_.containsKey(name)) return buttons_.get(name).state;
+			return false;
+		}
+		public double getAxis(String name) {
+			if (axes_.containsKey(name)) return axes_.get(name).state;
+			return 0.0;
+		}
+		
+		public void update() {
+			for (Button button : buttons_.values()) {
+				button.state = joystick_.getRawButton(button.num);
+			}
+			for (Axis axis : axes_.values()) {
+				axis.state = joystick_.getRawAxis(axis.num);
+			}
+		}
+	}
