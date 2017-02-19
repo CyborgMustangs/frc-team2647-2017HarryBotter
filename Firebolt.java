9103e3b722e10a6cada215e6ee38803fae10f4e7
Canis (Joshua Jacobson) <linktheherooftime2@gmail.com>
2017-02-19 03:26:53 -0700

    Made everything cleaner

---
+++ Firebolt.java
@@ -0,0 +1,74 @@
+package org.usfirst.frc.team2647.robot;
+
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+// named after Harry's broomstick
+public class Firebolt {
+	private CANTalon frontLeft;
+	private CANTalon rearLeft;
+	private CANTalon frontRight;
+	private CANTalon rearRight;
+	private RobotDrive drive;
+	
+	//Squared scaling - creates a smoother driving experience by squaring input values.
+	private boolean squaredScalingEnabled = false;
+	public boolean getsquaredScalingEnabled(){
+		return squaredScalingEnabled;
+	}
+	public void setsquaredScalingEnabled(boolean enabled){
+		squaredScalingEnabled = enabled;
+	}
+	
+	public Firebolt(int frontLeftPort, int frontRightPort, int rearLeftPort, int rearRightPort){
+		frontLeft = new CANTalon(frontLeftPort);
+		rearLeft = new CANTalon(rearLeftPort);
+		frontRight = new CANTalon(frontRightPort);
+		rearRight = new CANTalon(rearRightPort);
+		frontLeft.enable();
+		//rearLeft.setInverted(true);
+		rearLeft.enable();
+		frontRight.enable();
+		//rearRight.setInverted(true);
+		rearRight.enable();
+		drive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
+		drive.setSafetyEnabled(false);
+	}
+	
+	public void tankdrive(Joy joy){
+		//bounds checking
+		double leftVel = joy.getAxis("lDrive") * 0.7;
+		double rightVel = joy.getAxis("rDrive") * 0.7;
+		if(leftVel < -1.0) leftVel = -1.0;
+		else if(leftVel > 1.0) leftVel = 1.0;
+		if(rightVel < -1.0) rightVel = -1.0;
+		else if(rightVel > 1.0) rightVel = 1.0;
+		
+		drive.tankDrive(leftVel, rightVel, squaredScalingEnabled);
+	}
+	
+	public void arcadeDrive(double speed, double rot){
+		//bounds checking
+		if(speed < -1.0) speed = -1.0;
+		else if(speed > 1.0) speed = 1.0;
+		if(rot < -1.0) rot = -1.0;
+		else if(rot > 1.0) rot = 1.0;
+				
+		drive.arcadeDrive(speed, rot, squaredScalingEnabled);
+	}
+	
+	public void test(boolean port1on, boolean port2on, boolean port3on, boolean port4on){
+		if(port1on) frontLeft.set(0.5);
+		else frontLeft.set(0.0);
+		
+		if(port2on) rearLeft.set(0.5);
+		else rearLeft.set(0.0);
+		
+		if(port3on) frontRight.set(0.5);
+		else frontRight.set(0.0);
+		
+		if(port4on) rearRight.set(0.5);
+		else rearRight.set(0.0);
+	}
+	
+}
---
+++ Robot.java
@@ -0,0 +1,140 @@
+package org.usfirst.frc.team2647.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/*unused imports
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.CANTalon;*/
+
+import org.usfirst.frc.team2647.robot.Firebolt;
+import org.usfirst.frc.team2647.robot.SnitchPitch;
+import org.usfirst.frc.team2647.robot.HouseGearfindor;
+import org.usfirst.frc.team2647.robot.Leviosa;
+import org.usfirst.frc.team2647.robot.Input;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	
+	Input input = Input.getInstance();
+	
+	// Xbox 360 gamepad
+	final Joy X360 = input.getJoy(2);
+	
+	static final int xb_A = 1, xb_B = 2, xb_X = 3, xb_Y = 4,
+			 xb_LB = 5, xb_RB = 6, xb_SELECT = 7, xb_START = 8,
+			 xb_LSTICK = 9, xb_RSTICK = 10;
+	static final int xb_LSTICKX = 0, xb_LSTICKY = 1,
+			 xb_LT = 2, xb_RT = 3,
+			 xb_RSTICKX = 4, xb_RSTICKY = 5;
+	
+	// Extreme 3D Joystick
+	final Joy X3D = input.getJoy(0);
+	
+	static final int x3d_X = 0, x3d_Y = 1, x3d_Z = 2, x3d_Slider = 3;
+	
+	// Drivetrain
+	Firebolt drivetrain;
+	
+	// Shooter
+	SnitchPitch shooter;
+	
+	// Gear Box
+	HouseGearfindor gearBox;
+	
+	// Intake
+	Basilisk feeder;
+	
+	// Climber
+	Leviosa climber;
+	
+	public void initControllers() {
+	//When this is done with, be sure to only init the buttons you need or you'll be wasting CPU cycles!!!
+		
+	//xbox 360
+		X360.setButton("feedIn", xb_LB);
+		X360.setButton("feedOut", xb_RB);
+		
+		X360.setAxis("lDrive", xb_LSTICKY);
+		X360.setAxis("rDrive", xb_RSTICKY);
+	//extreme 3d joystick
+		X3D.setButton("controlledFire", 1);
+		X3D.setButton("flyFor", 5);
+		X3D.setButton("flyBack", 6);
+		X3D.setButton("pistFor", 3);
+		X3D.setButton("pistBack", 4);
+		X3D.setButton("climbUp", 8);
+		X3D.setButton("climbDown", 7);
+		
+		X3D.setAxis("doorPos", x3d_Slider);
+	}
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		drivetrain = new Firebolt(1, 2, 3, 4); // CAN ports. Front left motor, rear left motor, front right motor, rear right motord	
+		shooter = new SnitchPitch(0, 1, 2); // PWN ports. Top motor, bottom motor, piston motor.
+		gearBox = new HouseGearfindor(3,4); // PWM ports. Left door servo, right door servo.
+		feeder = new Basilisk(5);
+		climber = new Leviosa(6);
+		initControllers();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		// Input Checking
+		input.update();
+		drivetrain.tankdrive(X360);
+		shooter.shoot(X3D);
+		gearBox.setDoors(X3D);
+		climber.climb(X3D);
+		feeder.intake(X360);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+	
+	public void disabled(){
+		
+	}
+}
+
