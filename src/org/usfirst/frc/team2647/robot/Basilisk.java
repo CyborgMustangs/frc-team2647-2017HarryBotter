52116f53dcecdab9a9e59cbdf2468048e2409d38
Canis (Joshua Jacobson) <linktheherooftime2@gmail.com>
2017-02-19 03:06:51 -0700

    Also because why not.

--- src/org/usfirst/frc/team2647/robot/Basilisk.java
+++ src/org/usfirst/frc/team2647/robot/Basilisk.java
@@ -12,9 +12,11 @@ public class Basilisk {
 	
 	public void intake(Input input, int joyPort){
 		boolean in = input.getJoy(joyPort).getButton("feedIn");
-		boolean out = input.getJoy(joyPort).getButton("feedOut");
 		if(in) feeder.set(-1.0);
-		else if(out) feeder.set(1.0);
-		else feeder.set(0.0);
+		else {
+			boolean out = input.getJoy(joyPort).getButton("feedOut");
+			if(out) feeder.set(1.0);
+			else feeder.set(0.0);
+		}
 	}
 }
